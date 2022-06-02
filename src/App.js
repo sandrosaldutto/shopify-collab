2921e6b2cb28a2bbe7cef1f7f570ba5df7a56749
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-06-02 12:22:29 -0400

    completed collaborators page for mobile view

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.scss';
 import Nav from './components/Nav/Nav';
 import Categories from './components/Categories/Categories';
 import Collaborators from './pages/Collaborators/Collaborators';
-import Footer from './components/Footer/Footer';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 
@@ -15,7 +14,6 @@ function App() {
           <Route path='/' exact component={Categories}/>
           <Route path='/collaborators' component={Collaborators}/>
         </Switch>
-       <Footer />
       </BrowserRouter>
       
     </div>
